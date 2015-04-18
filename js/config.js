835b5a2bc22d0eac019d3f98c58bb81474cb0c38
Ben Purdy <ben.purdy@gmail.com>
2015-04-17 19:22:43 -0700

    Adding dynamic stuff and content updates.

---
+++ js/config.js
@@ -0,0 +1,6 @@
+var config = {
+	'base-url'          : 'http://localhost:3000',
+	'projector-on-url'  : '/unblank',
+	'projector-off-url' : '/blank',
+	'plumber-url'       : 'http://raspberrypi:8080/controls.html'
+};
