5782e687b624458a1ec27031accff9b51c6e20ee
Ben Purdy <ben.purdy@gmail.com>
2015-04-19 13:47:04 -0700

    Content updates, adding interactive parts

---
+++ GruntFile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),    
+    execute: {
+        target: {
+            src: ['generate-index.js']
+        }
+    },
+    watch: {
+      scripts: {
+        files: ['views/**/*.html'],
+        tasks: ['execute'],
+        options: {
+          spawn: false
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-execute');
+
+  
+  grunt.registerTask('default', ['watch']);
+};
--- js/config.js
+++ js/config.js
@@ -1,6 +1,9 @@
 var config = {
-	'base-url'          : 'http://localhost:3000',
-	'projector-on-url'  : '/unblank',
-	'projector-off-url' : '/blank',
-	'plumber-url'       : 'http://raspberrypi:8080/controls.html'
+	'base-url'              : 'http://localhost:3000',
+	'projector-on-url'      : '/unblank',
+	'projector-off-url'     : '/blank',
+	'plumber-url'           : 'http://raspberrypi:8080/controls.html',
+	'mindwave-url'          : 'some url',
+	'maptastic-socket-url'  : 'http://localhost:9000',
+	'uncontext-example-url' : 'http://codepen.io/ThisIsJohnBrown/embed/AqzuC' // todo: replace with codepen url
 };
